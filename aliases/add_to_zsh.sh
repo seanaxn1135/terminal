cbb7d0538c4175f681b7bc0c3c54b05473c61606
seanaxn1135 <seanaxn1135@gmail.com>
2023-02-21 23:27:20 +0800

    delete script

--- aliases/add_to_zsh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "running"
-
-# Get the path of the current script directory
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-# Create the symlink
-ln -sf "${SCRIPT_DIR}/alias.zsh" "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/alias.zsh"
-
