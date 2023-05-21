2337bb4c7527a8809c27ac235f4f1815b116da97
seanaxn1135 <seanaxn1135@gmail.com>
2023-05-21 22:04:38 +0800

    chore: change killport alias

--- aliases/aliases.zsh
+++ aliases/aliases.zsh
@@ -2,9 +2,7 @@
 alias test='echo "test"'
 
 # Kill a process running on a specific port
-function killport() {
-  lsof -i :$1 | awk '{print $2}' | tail -n 1 | xargs kill
-}
+alias killport='lsof -i :$1 | awk '\''{print $2}'\'' | tail -n 1 | xargs kill'
 
 # Search bash history
 alias history='history | grep'
